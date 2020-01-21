ddf4010159d875f17173037ddda53fc44b828ddd
Schuansk Lucas Tenorio Torres <schuansk@hotmail.com.br>
2020-01-20 22:03:40 -0300

    add backend

---
+++ backend/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const http = require('http')
+const routes = require('./routes')
+const { setupWebsocket } = require('./websocket')
+
+const app = express()
+const server = http.Server(app)
+
+setupWebsocket(server)
+
+mongoose.connect('mongodb+srv://xuxu:minhasenhamuitoforte@cluster0-d4n2g.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+// MongoDB (Não-relacionado)
+
+server.listen(3333)
